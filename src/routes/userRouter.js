d6a4aa03bc84e68ba9d8a3f8fe13a0ce676a9904
vinudasenith <ha.vinudas@gmail.com>
2025-12-03 21:19:04 +0530

    feat:protect user routes with JWT✌️

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { registerUser, loginUser, getAllUsers } from '../controllers/userController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const userRouter = express.Router();
+
+userRouter.use(authMiddleware);
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
