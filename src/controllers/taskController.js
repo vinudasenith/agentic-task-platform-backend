3f69798f699770ebf04f76d652a1fba5a17d80d8
vinudasenith <ha.vinudas@gmail.com>
2025-12-03 20:01:57 +0530

    feat(task):add get all task functionality✌️

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -17,3 +17,22 @@ export async function createTask(req, res) {
     }
 }
 
+// Get all tasks (user or admin)
+export async function getTasks(req, res) {
+    try {
+        let tasks;
+        if (req.user.role === "admin") {
+
+            tasks = await Task.find().populate("createdBy", "firstName lastName email");
+        } else {
+
+            tasks = await Task.find({ createdBy: req.user._id });
+        }
+
+        res.json(tasks);
+    } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).json({ error: "Failed to fetch tasks" });
+    }
+}
+
