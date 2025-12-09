89c2e38371e0f2c7e7e8c497e45d252e72c7fdc8
vinudasenith <ha.vinudas@gmail.com>
2025-12-09 06:25:54 +0530

    feat(user):add routes for get logged user data,delete logged user✌️

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getAllUsers } from '../controllers/userController.js';
+import { registerUser, loginUser, getAllUsers, getLoggedUser, deleteUserAccount } from '../controllers/userController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const userRouter = express.Router();
@@ -8,7 +8,12 @@ const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 
+
 // private routes
 userRouter.get('/all', authMiddleware, getAllUsers);
+userRouter.get('/me', authMiddleware, getLoggedUser);
+userRouter.delete('/deleteaccount', authMiddleware, deleteUserAccount);
+
+
 
 export default userRouter;
